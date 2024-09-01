070728453e5f68cb89c3388fc8b871bcbca602b2
brenno <brennorebeiro@gmail.com>
2024-09-01 12:46:08 -0300

    feat: add method getAllRooms

--- RoomService/src/main/java/com/spring/Controller/RoomController.java
+++ RoomService/src/main/java/com/spring/Controller/RoomController.java
@@ -1,9 +1,22 @@
 package com.spring.Controller;
 
+import com.spring.Dto.RoomDto;
+import com.spring.Service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
 public class RoomController {
+
+    @Autowired
+    private RoomService roomService;
+
+    public ResponseEntity<List<RoomDto>> getAllRooms(){
+        return ResponseEntity.ok(roomService.getAllRooms());
+    }
 }
