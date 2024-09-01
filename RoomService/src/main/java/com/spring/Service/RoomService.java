ae77e36ad4c28b75a147f39f4788e17918d3a032
brenno <brennorebeiro@gmail.com>
2024-09-01 12:42:16 -0300

    feat: add method getAllRooms

--- RoomService/src/main/java/com/spring/Service/RoomService.java
+++ RoomService/src/main/java/com/spring/Service/RoomService.java
@@ -1,11 +1,23 @@
 package com.spring.Service;
 
+import com.spring.Dto.RoomDto;
 import com.spring.Repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomService {
     @Autowired
     private RoomRepository roomRepository;
+
+    /**
+     * Return all the rooms that are registered
+     * @return all the rooms that are registered*/
+    public List<RoomDto> getAllRooms() {
+        return roomRepository.findAll()
+                .stream()
+                .map(RoomDto::new).toList();
+    }
 }
