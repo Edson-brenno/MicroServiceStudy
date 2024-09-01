7390586ed5f01c4a71fe631c29af7a739719462e
brenno <brennorebeiro@gmail.com>
2024-09-01 12:45:03 -0300

    feat: add RoomController

---
+++ RoomService/src/main/java/com/spring/Controller/RoomController.java
@@ -0,0 +1,9 @@
+package com.spring.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
+public class RoomController {
+}
