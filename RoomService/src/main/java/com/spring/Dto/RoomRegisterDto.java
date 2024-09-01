0eee98cf6793e0881466081972530e511ce562b4
brenno <brennorebeiro@gmail.com>
2024-09-01 12:40:48 -0300

    feat: add room register dto

---
+++ RoomService/src/main/java/com/spring/Dto/RoomRegisterDto.java
@@ -0,0 +1,7 @@
+package com.spring.Dto;
+
+/**
+ * This dto is responsible to have all the needed information to create
+ * a new room*/
+public record RoomRegisterDto (String name, Long capacity) {
+}
