b2d58c13e4cd4a37e665f2958684438b6fbdd396
brenno <brennorebeiro@gmail.com>
2024-09-01 12:32:48 -0300

    feat: add configuration scan

--- RoomService/src/main/java/com/spring/roomservice/RoomServiceApplication.java
+++ RoomService/src/main/java/com/spring/roomservice/RoomServiceApplication.java
@@ -1,9 +1,16 @@
 package com.spring.roomservice;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.spring.Entity")
+@EnableJpaRepositories(basePackages = "com.spring.Repository")
+@ComponentScan(basePackages = "com.spring")
 public class RoomServiceApplication {
 
 	public static void main(String[] args) {
