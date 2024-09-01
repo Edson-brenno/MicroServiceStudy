7e7cb20520c0658a6f4b4055a1d79a2dce485d4e
brenno <brennorebeiro@gmail.com>
2024-09-01 11:02:23 -0300

    add project RoomService

---
+++ RoomService/src/main/java/com/spring/roomservice/RoomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.roomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomServiceApplication.class, args);
+	}
+
+}
