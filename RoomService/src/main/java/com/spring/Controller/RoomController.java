925cb26752ac742d5e3d68e1c7bafe6f2ec0ea54
brenno <brennorebeiro@gmail.com>
2024-09-01 12:47:59 -0300

    feat: add method createRoom

--- RoomService/src/main/java/com/spring/Controller/RoomController.java
+++ RoomService/src/main/java/com/spring/Controller/RoomController.java
@@ -1,13 +1,11 @@
 package com.spring.Controller;
 
 import com.spring.Dto.RoomDto;
+import com.spring.Dto.RoomRegisterDto;
 import com.spring.Service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,10 @@ public class RoomController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<RoomDto> getRoomById(@PathVariable("id") Long id){
         return ResponseEntity.ok(roomService.getRoomById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<RoomDto> createRoom(@RequestBody RoomRegisterDto roomDto){
+        return ResponseEntity.ok().body(roomService.registerRoom(roomDto));
     }
 }
