9b843c7a675aa7f6fa6a9a8f57104e4a4885ff2b
brenno <brennorebeiro@gmail.com>
2024-09-01 12:47:17 -0300

    feat: add method getRoomById

--- RoomService/src/main/java/com/spring/Controller/RoomController.java
+++ RoomService/src/main/java/com/spring/Controller/RoomController.java
@@ -4,6 +4,8 @@ import com.spring.Dto.RoomDto;
 import com.spring.Service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,13 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
 
+    @GetMapping
     public ResponseEntity<List<RoomDto>> getAllRooms(){
         return ResponseEntity.ok(roomService.getAllRooms());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<RoomDto> getRoomById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(roomService.getRoomById(id));
     }
 }
